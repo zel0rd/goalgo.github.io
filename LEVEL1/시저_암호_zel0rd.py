77c8fe37b6bffc7f8aa0911b29fbd0c3981c16bb
ZELORD_MAC <zelord.kwoun@gmail.com>
2019-03-29 15:16:58 +0900

    시저_암호_zel0rd.py

--- LEVEL1/시저_암호_zel0rd.py
+++ LEVEL1/시저_암호_zel0rd.py
@@ -6,16 +6,20 @@ Created on Sat Dec 22 15:42:32 2018
 @author: zel0rd
 """
 
+
 def solution(s, n):
-    list1 = list(s)
-    small = []
-    big = []
-    for i in range(len(list1)):
-        if(ord(list1[i]) >= 97 and ord(list1[i]) <= 122):
-            small.append(list1[i])
-        elif(ord(list1[i]) >= 65 and ord(list1[i]) <= 90):
-            big.append(list1[i])
     answer = ''
+    for i in range(len(s)):
+        if s[i].isupper():
+            temp = ord(s[i]) + n
+            if temp > 90:
+                temp = temp % 90 + 64
+            answer += chr(temp)
+        elif s[i].islower():
+            temp = ord(s[i]) + n
+            if temp > 122:
+                temp = temp % 122 + 96
+            answer += chr(temp)
+        else:
+            answer += ' '
     return answer
-
-
