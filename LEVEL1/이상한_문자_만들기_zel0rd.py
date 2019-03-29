1e08e8f0c94bd36d5be566343d520534f3808915
ZELORD_MAC <zelord.kwoun@gmail.com>
2019-03-30 01:04:32 +0900

    이상한_문자_만들기_zel0rd.py

---
+++ LEVEL1/이상한_문자_만들기_zel0rd.py
@@ -0,0 +1,12 @@
+def solution(s):
+    answer = ''
+    s = s.split(' ')
+    print(s)
+    for i in range(len(s)):
+        for j in range(len(s[i])):
+            if j % 2 == 0:
+                answer += s[i][j].upper()
+            else:
+                answer += s[i][j].lower()
+        answer += ' '
+    return answer[:-1]
