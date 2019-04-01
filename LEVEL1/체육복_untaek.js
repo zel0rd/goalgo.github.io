e605711516969fef06fc7aad86868a1304778d1e
Untaek <wkblack11@gmail.com>
2019-04-01 12:35:13 +0900

    체육복 임시

---
+++ LEVEL1/체육복_untaek.js
@@ -0,0 +1,29 @@
+/**
+ *  체육복 
+ *  https://programmers.co.kr/learn/courses/30/lessons/42862
+ *  ** fail **
+ */
+
+function solution(n, lost, reserve) {
+    var answer = 0;
+    let students = new Array(n).fill(1);
+    lost.forEach(i => students[i-1] = 0)
+    reserve.forEach(i => students[i-1] = 2)
+    for(let i=0; i<n; i++) {
+        if(students[i] == 0) {
+            if(0 < i && i < n && students[i-1] == 2) {
+                students[i] = 1
+                students[i-1] = 1
+            }
+            else if(i < n-1 && students[i+1] == 2) {
+                students[i] = 1
+                students[i+1] = 1
+            }
+        }
+    }
+    
+    students.forEach(i => {
+        if(i == 2 || i == 1) answer++
+    })
+    return answer;
+}
