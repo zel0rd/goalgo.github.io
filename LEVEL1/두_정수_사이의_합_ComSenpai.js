6af2f48748da88795e31c66b8993f5e2fc9f007e
Jiho Han <mlpzaq123456@naver.com>
2019-04-01 17:02:20 +0900

    2019.04.01_ComSenpai
    두 정수 사이의 합 알고리즘(LEVEL 1)을
    C와 JS 두 가지 언어로 풀어보았습니다.

--- LEVEL1/두_정수_사이의_합_ComSenpai.js
+++ LEVEL1/두_정수_사이의_합_ComSenpai.js
@@ -1,7 +1,9 @@
 function solution(a, b) {
-    var result = (a<b)?a:b;
-    while(a!=b){
+    var result = (a<b)?a:b; //a가 더 크다면 result == b
+
+    while(a!=b){ //a와 b가 같아질 때 까지 반복한다
       result += (a<b)?++a:++b;
     }    
+
     return result;
 }
