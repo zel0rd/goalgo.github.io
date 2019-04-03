226ed8349f082b636a1412167248b41f507f6a2d
INJE YUN <34809802+yuninje@users.noreply.github.com>
2019-04-04 00:55:03 +0900

    Update 완주하지_못한_선수_inje.java

--- LEVEL1/완주하지_못한_선수_inje.java
+++ LEVEL1/완주하지_못한_선수_inje.java
@@ -1,17 +1,13 @@
 public static String solution(String[] participant, String[] completion) {
-        String answer = "";
         Arrays.sort(participant);
         Arrays.sort(completion);
+        String answer = participant[participant.length-1];
         for(int i = 0; i<completion.length;i++){
             if(!participant[i].equals(completion[i])){
                 answer = participant[i];
                 break;
             }
         }
-        if(answer.equal("")){
-            answer = participant[participant.length-1];
-        }
-
 
         System.out.println(answer);
         return answer;
