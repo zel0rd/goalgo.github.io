9bcf6ccb4e8430f89495d315bba2223908bf41d8
sejunoh <dhthwn@gmail.com>
2019-04-04 18:01:33 +0900

    탑_sejunoh.java

---
+++ LEVEL2/탑_sejunoh.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] solution(int[] heights) {
+        int[] answer = new int[heights.length];
+        for(int i= heights.length-1; i>0; i--){
+            for(int j=i-1;j >-1;j--){
+                if(heights[j]>heights[i]){
+                    answer[i]= j+1;
+                    break;
+                }
+            }
+        }
+        return answer;
+    }
+}
